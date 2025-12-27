23b53e7215381f3fb394c84ee6bf64f49bf10730
Shimanta Chowdhury <scshomu@gmail.com>
2025-12-28 00:11:49 +0600

    tutor profile upgrade 2

--- src/Pages/TutorProfile/TutorProfile.jsx
+++ src/Pages/TutorProfile/TutorProfile.jsx
@@ -15,7 +15,7 @@ const TutorProfile = () => {
 
   const fetchTutorProfile = async () => {
     try {
-      const response = await fetch(`${apiUrl}/api/users/${id}`);
+      const response = await fetch(`${apiUrl}/api/users/role/tutor`);
       if (response.ok) {
         const data = await response.json();
         setTutor(data);
